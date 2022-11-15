7497a7a890d3e099a987f9d8614cf690d61af3c0
aitajorge <aitajorge@yahoo.com.ar>
2022-11-15 16:57:40 -0300

    subo a repositorio, primera vez

---
+++ app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-raiz',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'miAppPortfDinam';
+
+  saludo="Hola alumnos de Angular, cómo están?";
+}
